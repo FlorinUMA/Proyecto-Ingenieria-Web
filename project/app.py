ae023c9e21688f52f4fbe70bacc7efe901934c6b
JCruiz15 <juancaruru@uma.es>
2022-11-29 20:14:06 +0100

    Created base and index, app.py updated

--- project/app.py
+++ project/app.py
@@ -8,8 +8,16 @@ from flask import (
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def home():
-    return render_template("base.jinja")
+def index():
+    return render_template("index.jinja")
+
+@app.route("/error", methods=["GET"])
+def error():
+    return render_template("error.jinja")
+
+# @app.route("/asig-tarea", methods=["POST"])
+# def asig_tarea():
+#     pass
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)

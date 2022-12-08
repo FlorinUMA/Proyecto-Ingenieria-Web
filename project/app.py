07a18e97c7bafc3007aab87e25e84f0b38a3d322
JCruiz15 <juancaruru@uma.es>
2022-12-08 11:15:12 +0100

    Changes into bd implementation

--- project/app.py
+++ project/app.py
@@ -6,35 +6,45 @@ from flask import (
     request
 )
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.pool import StaticPool
+
+engine = create_engine(
+    "sqlite://", 
+    connect_args={"check_same_thread": False}, 
+    poolclass=StaticPool
+)
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db' # 3 barras porque es un path relativo. SI fuera absoluto, serían 4 barras
 with app.app_context():
     db = SQLAlchemy(app)
 
-class Personal(db.Model):
-    __tablename__ = 'Personal'
+class Usuario(db.Model):
+    __tablename__ = 'Usuario'
     usuario = db.Column(db.String(50), primary_key=True)
     contrasenya = db.Column(db.String(200), nullable=False)
+    rol = db.Column(db.String(10), nullable=False)
     def __repr__(self):
         return 'Usuario %r' % self.usuario
 
-class Medicos(db.Model):
-    __tablename__ = 'Medicos'
-    usuarioMedico = db.Column(db.String(50), db.ForeignKey(Personal.usuario), primary_key=True)
-    def __repr__(self):
-        return 'Medico %r' % self.usuarioMedico
+# class Medicos(db.Model):
+#     __tablename__ = 'Medicos'
+#     usuarioMedico = db.Column(db.String(50), db.ForeignKey(Personal.usuario), primary_key=True)
+#     def __repr__(self):
+#         return 'Medico %r' % self.usuarioMedico
 
-class Tecnicos(db.Model):
-    __tablename__ = 'Tecnicos'
-    usuarioTecnico = db.Column(db.String(50), db.ForeignKey(Personal.usuario), primary_key=True)
-    def __repr__(self):
-        return 'Tecnico %r' % self.usuarioMedico
+# class Tecnicos(db.Model):
+#     __tablename__ = 'Tecnicos'
+#     usuarioTecnico = db.Column(db.String(50), db.ForeignKey(Personal.usuario), primary_key=True)
+#     def __repr__(self):
+#         return 'Tecnico %r' % self.usuarioMedico
 
 class Robots(db.Model):
     __tablename__ = 'Robots'
     id = db.Column(db.Integer, primary_key=True)
-    us_Tec = db.Column(db.String(50), db.ForeignKey(Tecnicos.usuarioTecnico), nullable = False)
+    # us_Tec = db.Column(db.String(50), db.ForeignKey(Tecnicos.usuarioTecnico), nullable = False)
     def __repr__(self):
         return 'Robot %r' % self.id
 
@@ -55,6 +65,23 @@ class Tareas(db.Model):
     def __repr__(self):
         return 'Tarea %r' % self.nombre + " " + self.rob_Id
 
+
+
+def inserta_usuarios():
+    medico1 = Usuario(usuario="medico1", contrasenya="12345", rol="medico")
+    medico2 = Usuario(usuario="medico2", contrasenya="11111", rol="medico")
+    tecnico1 = Usuario(usuario="tecnico1", contrasenya="0000", rol="tecnico")
+    tecnico2 = Usuario(usuario="tecnico2", contrasenya="soy_tecnico", rol="tecnico")
+
+    db.session.add(medico1)
+    db.session.commit()
+    db.session.add(medico2)
+    db.session.commit()
+    db.session.add(tecnico1)
+    db.session.commit()
+    db.session.add(tecnico2)
+    db.session.commit()
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.jinja")
@@ -62,8 +89,14 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        print("post received")
-        print(request.get_json())
+        # print("post received")
+        user = request.form.get("user")
+        passw = request.form.get("password")
+
+        query = Usuario.query.first()
+        print(query.contrasenya)
+
+
         return redirect("/")
     else:
         return render_template("login.jinja")
@@ -73,4 +106,7 @@ def login():
 #     pass
 
 if __name__ == "__main__":
+    app.app_context().push()
+    db.create_all()
+    inserta_usuarios()
     app.run(port=5000, debug=True)
